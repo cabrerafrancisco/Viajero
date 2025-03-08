e76a1957837d1d1e62ee537f0a0061d89f7b6ca4
Francisco Cabrera <francabre97@gmail.com>
2025-03-08 03:19:00 -0300

    Logica de JS para cards dinamicas y consumo de JSON

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("data/place.json")
+        .then(Response => Response.json())
+        .then(data => renderCities(data))
+        .then(error => console.error("Error al cargar ciudades" , error));
+});
+
+function renderCities(cities) {
+    const citiesContainer = document.querySelector(".cities");
+
+    cities.forEach(city => {
+        const cityCard = document.createElement("div");
+        cityCard.classList.add("card");
+
+        cityCard.innerHTML = `
+        <img src="${city.img}" alt="${city.city}">
+            <h2>${city.city}</h2>
+            <p>${city.desc}</p>
+            <p><strong>Precio base: $${city.price}</strong></p>
+            <button onclick="selectCity(${city.id})">Ver más</button>
+        `;
+
+        citiesContainer.appendChild(cityCard);        
+    });
+}
+
+function selectCity(cityId){
+    localStorage.setItem("selectedCity", cityId);
+    window.location.href = "detalle.html";
+}
